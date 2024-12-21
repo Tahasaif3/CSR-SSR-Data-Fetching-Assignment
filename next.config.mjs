bf5dde3a60c515b66e554bec1f9a8b0e6969e4bf
Taha Saif <tahasaifsurhio454@gmail.com>
2024-12-21 11:46:03 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['fakestoreapi.com'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'fakestoreapi.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
     },
-  }
+  };
   
-  module.exports = nextConfig
+  export default nextConfig;
   
   
